b34c6ff2ecfe2ded67c18e425db63508f6e2145f
bbader <brianbader@mail.com>
2015-11-03 18:09:53 -0500

    fixed stability issues w/ gevr profile likelihood

--- R/gevr.returnlevel.R
+++ R/gevr.returnlevel.R
@@ -6,13 +6,13 @@
 #'@param period The number of periods to use for the return level.
 #'@param conf Confidence level. Defaults to 95 percent.
 #'@param method The method to compute the confidence interval - either delta method (default) or profile likelihood.
-#'@param opt Optimization method to maximize the profile likelihood if that is selected. The default (SANN) is slow, but works well.
+#'@param opt Optimization method to maximize the profile likelihood if that is selected. The default method is Nelder-Mead.
 #'
 #'@references http://www.mas.ncl.ac.uk/~nlf8/teaching/mas8391/background/chapter2.pdf
 #'@examples
-#'data <- rgevr(100, 5, loc=0.5, scale=1, shape=0.3)
+#'data <- rgevr(100, 2, loc=0.5, scale=1, shape=0.3)
 #'## Compute 50-period return level.
-#'gevr.returnlevel(data, 50)
+#'gevr.returnlevel(data, 50, method = "profile")
 #'@return Estimate Estimated m-period return level.
 #'@return CI Confidence interval for the m-period return level.
 #'@return Period The period length used.
@@ -21,24 +21,24 @@
 #'@export
 
 gevr.returnlevel <- function(data, period, conf=.95, method=c("delta", "profile"),
-                             opt=c("SANN", "Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "Brent"))
+                             opt=c("Nelder-Mead", "SANN", "BFGS", "CG", "L-BFGS-B", "Brent"))
 {
   method <- match.arg(method)
   data <- as.matrix(data)
-  y <- 9999
-  try(y <- rlarg.fit(data, show=FALSE), silent = FALSE)
+  y <- NA
+  try(y <- rlarg.fit(data, show = FALSE), silent = TRUE)
   if (!is.list(y))
     stop("Maximum likelihood failed to converge at initial step")
   cov <- y$cov
   theta <- y$mle
-  z <- -log(1-(1/period))
-  est <- theta[1] - (theta[2]/theta[3])*(1-(z^(-theta[3])))
+  z <- -log1p(-(1/period))
+  est <- theta[1] - (theta[2]/theta[3])*(-expm1(-theta[3]*log(z)))
   if(method == "delta") {
     del <- matrix(ncol=1, nrow=3)
     del[1,1] <- 1
-    del[2,1] <- -((theta[3])^(-1))*(1-(z^(-theta[3])))
-    del[3,1] <- ((theta[2])*((theta[3])^(-2))*(1-((z)^(-theta[3])))) -((theta[2])*((theta[3])^(-1))*((z)^(-(theta[3])))*log(z))
-    se <- sqrt(t(del)%*%cov%*%del)
+    del[2,1] <- -((theta[3])^(-1))*(-expm1(-theta[3]*log(z)))
+    del[3,1] <- ((theta[2])*(theta[3]^(-2))*(-expm1(-theta[3]*log(z)))) - ((theta[2])*((theta[3])^(-1))*((1+expm1(-theta[3]*log(z)))*log(z)))
+    se <- sqrt(t(del) %*% cov %*% del)
     se <- as.vector(se)
     alpha <- (1-conf)/2
     lower <- est - qnorm(1-alpha)*se
@@ -49,40 +49,30 @@ gevr.returnlevel <- function(data, period, conf=.95, method=c("delta", "profile"
     opt <- match.arg(opt)
     sol <- c(theta[2], theta[3])
     gevr.lik <- function(a, xp) {
-      R <- ncol(data)
-      loc <- xp + (a[1]/a[2])*(1-z^(-a[2]))
-      y <- (a[2] / a[1]) * (data - loc)
-      w <- (1 / a[1]) * (data - loc)
-      if(a[2] == 0) {
-        log.density <- rowSums( -log(a[1]) - w )
-        log.density <- log.density - exp(-w[,R])
-        log.density <- sum(log.density)
+      loc <- xp + (a[1]/a[2])*(-expm1(-a[2]*log(z)))
+      if(a[1] <= 0){
+       out <- 1e6
       }
-      else {
-        log.density <- rowSums( -log(a[1]) - ((1/a[2]) + 1) * log1p(y) )
-        log.density <- log.density - (1 + y[,R])^(-1 / a[2])
-        log.density <- sum(log.density)
+      else{
+        out <- dgevr(data, loc = loc, scale = a[1], shape = a[2], log.d = TRUE)
+        out <- - sum(out)
+        if(out == Inf) out <- 1e6
       }
-      -log.density
+      out
     }
     cutoff <- qchisq(conf, 1)
     prof <- function(xp){
-      lmax <- dgevr(data, theta[1], theta[2], theta[3], log.d=TRUE)
+      lmax <- dgevr(data, theta[1], theta[2], theta[3], log.d = TRUE)
       lmax <- sum(lmax)
-      yes <- optim(sol, gevr.lik, method=opt, xp=xp)
+      yes <- optim(sol, gevr.lik, method = opt, xp = xp)
       sol <- yes$par
       lci <- -yes$value
       2*(lmax-lci) - cutoff
     }
-
     prof <- Vectorize(prof)
-    suppressWarnings( out1 <- uniroot(prof, c(est-0.000001, est), extendInt="downX") )
-    suppressWarnings( out2 <- uniroot(prof, c(est, est+0.000001), extendInt="upX") )
-    out1 <- as.vector(out1$root)
-    out2 <- as.vector(out2$root)
-    lower <- min(out1, out2)
-    upper <- max(out1, out2)
-    CI <- c(lower, upper)
+    suppressWarnings(out1 <- uniroot(prof, c(est - 1e-6, est), extendInt="downX"))
+    suppressWarnings(out2 <- uniroot(prof, c(est, est + 1e-6), extendInt="upX"))
+    CI <- c(min(out1$root, out2$root), max(out1$root, out2$root))
   }
   out <- list(est, CI, period)
   names(out) <- c("Estimate", "CI", "Period")
