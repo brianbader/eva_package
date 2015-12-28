8ac6afaf44df819f906bdcc9a0e0717385eba36e
bbader <brianbader@mail.com>
2015-12-28 10:09:06 -0500

    documentation updates

--- R/pSeqStop.R
+++ R/pSeqStop.R
@@ -3,6 +3,7 @@
 #' Given a set of (ordered) p-values, returns p-values adjusted according to the ForwardStop and StrongStop stopping rules.
 #' @param p Vector of ordered p-values.
 #' @references G'Sell, M. G., Wager, S., Chouldechova, A., & Tibshirani, R. (2013). Sequential Selection Procedures and False Discovery Rate Control. arXiv preprint arXiv:1309.5352.
+#' @references Benjamini, Y., & Hochberg, Y. (1995). Controlling the false discovery rate: a practical and powerful approach to multiple testing. Journal of the Royal Statistical Society. Series B (Methodological), 289-300.
 #' @references Bader B., Jun Y., & Zhang X. (2015). Automated Selection of r for the r Largest Order Statistics Approach with Adjustment for Sequential Testing. Department of Statistics, University of Connecticut.
 #' @examples
 #' x <- rgevr(500, 10, loc = 0.5, scale = 1, shape = 0.5)
@@ -11,6 +12,8 @@
 #' @return StrongStop Vector of ordered p-values adjusted for the familywise error rate.
 #' @return ForwardStop Vector of ordered p-values adjusted for the false discovery rate.
 #' @return UnAdjusted Vector of non-transformed p-values.
+#' @details Roughly speaking, under the assumption of independent but ordered p-values, the StrongStop adjusted p-values
+#' control for the familywise error rate, while ForwardStop provides control for the false discovery rate.
 #' @export
 pSeqStop <- function(p) {
   m <- length(p)
