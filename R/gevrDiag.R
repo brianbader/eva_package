1201350a0d2a6ef353976e7b43dcae8caf07d097
bbader <brianbader@mail.com>
2016-03-17 15:03:46 -0400

    minor edits

--- R/gevrDiag.R
+++ R/gevrDiag.R
@@ -18,11 +18,8 @@ gevrRlPlot <- function(z, conf = 0.95, method = c("delta", "profile")) {
 }
 
 
-pgevMarg1 <- function(z, j) {
+pgevMarg1 <- function(z, j, locvec, scalevec, shapevec) {
   p <- rep(0, z$n)
-  locvec <- z$links[[1]](rowSums(t(z$par.ests[1:z$parnum[1]] * t(z$covars[[1]]))))
-  scalevec <- z$links[[2]](rowSums(t(z$par.ests[(z$parnum[1] + 1):(z$parnum[1] + z$parnum[2])] * t(z$covars[[2]]))))
-  shapevec <- z$links[[3]](rowSums(t(z$par.ests[(z$parnum[1] + z$parnum[2] + 1):(z$parnum[1] + z$parnum[2] + z$parnum[3])] * t(z$covars[[3]]))))
   for(i in 1:z$n) {
     for(k in 0:(j-1)) {
       p[i] <- p[i] + nzsh((z$data[i, j] - locvec[i]) / scalevec[i], shapevec[i])^k / gamma(k+1)
@@ -33,11 +30,8 @@ pgevMarg1 <- function(z, j) {
 }
 
 
-pgevMarg2 <- function(x, z, j, i) {
+pgevMarg2 <- function(x, z, j, i, locvec, scalevec, shapevec) {
   p <- 0
-  locvec <- z$links[[1]](rowSums(t(z$par.ests[1:z$parnum[1]] * t(z$covars[[1]]))))
-  scalevec <- z$links[[2]](rowSums(t(z$par.ests[(z$parnum[1] + 1):(z$parnum[1] + z$parnum[2])] * t(z$covars[[2]]))))
-  shapevec <- z$links[[3]](rowSums(t(z$par.ests[(z$parnum[1] + z$parnum[2] + 1):(z$parnum[1] + z$parnum[2] + z$parnum[3])] * t(z$covars[[3]]))))
   for(k in 0:(j-1)) {
     p <- p + nzsh((x - locvec[i]) / scalevec[i], shapevec[i])^k / gamma(k+1)
   }
@@ -45,15 +39,12 @@ pgevMarg2 <- function(x, z, j, i) {
 }
 
 
-gevrQQ <- function(z, j) {
+gevrQQ <- function(z, j, locvec, scalevec, shapevec) {
   qgevMarg <- function(x, q, i) {
-    q - pgevMarg2(x, z, j, i)
+    q - pgevMarg2(x, z, j, i, locvec, scalevec, shapevec)
   }
   emp <- rep(0, z$n)
   Series <- seq(1, z$n, 1) / (z$n + 1)
-  locvec <- z$links[[1]](rowSums(t(z$par.ests[1:z$parnum[1]] * t(z$covars[[1]]))))
-  scalevec <- z$links[[2]](rowSums(t(z$par.ests[(z$parnum[1] + 1):(z$parnum[1] + z$parnum[2])] * t(z$covars[[2]]))))
-  shapevec <- z$links[[3]](rowSums(t(z$par.ests[(z$parnum[1] + z$parnum[2] + 1):(z$parnum[1] + z$parnum[2] + z$parnum[3])] * t(z$covars[[3]]))))
   ztrans <- nzsh(-(z$data[, j] - locvec) / scalevec, - shapevec)
   Series <- Series[order(Series)[rank(ztrans)]]
   for(i in 1:z$n) {
@@ -70,10 +61,10 @@ gevrQQ <- function(z, j) {
 
 
 ## Provide it the fitted object and which marginal statistic to plot (j)
-gevrPP <- function(z, j) {
+gevrPP <- function(z, j, locvec, scalevec, shapevec) {
   n <- z$n
   Series <- seq(1, z$n, 1) / (z$n + 1)
-  p <- pgevMarg1(z, j)
+  p <- pgevMarg1(z, j, locvec, scalevec, shapevec)
   p <- sort(p)
   plot(p, Series, xlab = "Empirical", ylab = "Model", xlim = c(0,1), ylim = c(0,1))
   if(z$stationary)
@@ -117,7 +108,7 @@ gevrHist <- function(z, j) {
 #' ## Not run
 #' # x <- rgevr(500, 2, loc = 0.5, scale = 1, shape = 0.1)
 #' # z <- gevrFit(x)
-#' # gevrDiag(z)
+#' # plot(z)
 #' @return For stationary models, provides return level plot and density, probability,
 #' and quantile plots for each marginal order statistic. The overlaid density is the 'true' marginal
 #' density for the estimated parameters. For nonstationary models, provides only residual probability and quantile plots.
@@ -128,13 +119,16 @@ gevrHist <- function(z, j) {
 gevrDiag <- function(z, conf = 0.95, method = c("delta", "profile")) {
   method <- match.arg(method)
   par(ask = TRUE, mfcol = c(2, 2))
+  locvec <- z$links[[1]](rowSums(t(z$par.ests[1:z$parnum[1]] * t(z$covars[[1]]))))
+  scalevec <- z$links[[2]](rowSums(t(z$par.ests[(z$parnum[1] + 1):(z$parnum[1] + z$parnum[2])] * t(z$covars[[2]]))))
+  shapevec <- z$links[[3]](rowSums(t(z$par.ests[(z$parnum[1] + z$parnum[2] + 1):(z$parnum[1] + z$parnum[2] + z$parnum[3])] * t(z$covars[[3]]))))
   if(z$stationary)
     try(gevrRlPlot(z, conf, method), silent = TRUE)
   for(i in 1:z$R) {
     if(z$stationary)
       try(gevrHist(z, i), silent = TRUE)
-    try(gevrPP(z, i), silent = TRUE)
-    try(gevrQQ(z, i), silent = TRUE)
+    try(gevrPP(z, i, locvec, scalevec, shapevec), silent = TRUE)
+    try(gevrQQ(z, i, locvec, scalevec, shapevec), silent = TRUE)
   }
   par(mfrow = c(1, 1))
 }
